959ad7ded420b20cf7cee8993c85dd565c5387f0
unknown <ianchen6501@gmail.com>
2020-10-29 16:04:14 +0800

    css and js minify by gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,39 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var { src, dest, series, parallel} = require ('gulp');
+var uglify = require('gulp-uglify');
+var cleanCSS = require('gulp-clean-css'); //css 壓縮
+var rename = require('gulp-rename') 
+sass.compiler = require('node-sass');
+
+function compileJS(cb) {
+  return src('src/*.js')
+  .pipe(uglify())
+  .pipe(rename({extname: '.min.js'}))
+  .pipe(dest('dist'))
+  cb()
+}
+
+function SASS() {
+  return src('scss/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(dest('./css'));
+}
+
+function minifyCSS(cb) {
+  return  gulp.src('css/*.css')
+  .pipe(cleanCSS({compatibility: 'ie8'}))
+  .pipe(gulp.dest('clean-css'));
+}
+
 function defaultTask(cb) {
   // place code for your default task here
   cb();
 }
 
+
 exports.default = defaultTask
+exports.SASS = SASS
+exports.minifyCSS = minifyCSS
+exports.compileCSS = series(SASS, minifyCSS)
+exports.compileJS = compileJS
